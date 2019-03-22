d8d6edb4811820c2881fcd6c87ff8fc8168efd42
Priyadarshi Roy <37141090+Priyadarshiroy@users.noreply.github.com>
2019-03-22 20:13:38 +0530

    Final design changes to slider app (proy)

--- sliders.py
+++ sliders.py
@@ -40,11 +40,11 @@ flag = 1
 timer_val = 3
 
 event = None
-model_alpha = load_model('model/extended_atoz_2.h5')
-model_num = load_model('model/extended_0to9_2.h5')
+# model_alpha = load_model('model/extended_atoz_2.h5')
+# model_num = load_model('model/extended_0to9_2.h5')
 
-model = model_alpha
-model_text = 'Alphabetic model'
+# model = model_alpha
+# model_text = 'Alphabetic model'
 class KivyCamera(Image):
     #init function to initialize the capture variable
     def __init__(self, **kwargs):
@@ -183,6 +183,13 @@ class HslSliderApp(GridLayout):
     thresh_lbl = ObjectProperty(None)
     sentence = ObjectProperty(None)
 
+    u_hue_lbl = ObjectProperty(None)
+    u_sat_lbl = ObjectProperty(None)
+    u_val_lbl = ObjectProperty(None)
+    l_hue_lbl = ObjectProperty(None)
+    l_sat_lbl = ObjectProperty(None)
+    l_val_lbl = ObjectProperty(None)
+
     def __init__(self, **kwargs):
         super(HslSliderApp, self).__init__(**kwargs)
         global flag
@@ -204,55 +211,55 @@ class HslSliderApp(GridLayout):
         return model_text
 
 
-    def predict_model(self,mask):
-        mask = cv2.merge((mask, mask, mask))
-        gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(gray, (128, 128))
-        cv2.imshow('resized', img)
-        img = cv2.resize(gray, (64, 64))
-        img2 = img.reshape(1, 64, 64, 1)
-        prediction = model.predict_classes(img2)
-        predict_prob = model.predict(img2)
-        prob = predict_prob[0][np.argmax(predict_prob[0])]
-        print(predict_prob[0][np.argmax(predict_prob[0])])
+    # def predict_model(self,mask):
+    #     mask = cv2.merge((mask, mask, mask))
+    #     gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    #     img = cv2.resize(gray, (128, 128))
+    #     cv2.imshow('resized', img)
+    #     img = cv2.resize(gray, (64, 64))
+    #     img2 = img.reshape(1, 64, 64, 1)
+    #     prediction = model.predict_classes(img2)
+    #     predict_prob = model.predict(img2)
+    #     prob = predict_prob[0][np.argmax(predict_prob[0])]
+    #     print(predict_prob[0][np.argmax(predict_prob[0])])
 
-        return prediction, prob
+    #     return prediction, prob
 
-    def predict(self):
-        # Predicting the output
-        global final_mask,model_text
-        prediction, prob = self.predict_model(final_mask)
-        if prob >= .80:
-            if model == model_alpha:
-                if prediction[0] == 26:
-                    model_text=self.model_switch(1)
-                if prediction[0] == 27:
-                    model_text=self.model_switch(2)
-                else:
-                    result = str(chr(prediction[0] + 65))
-                    # result = str(chr(result_map(str(prediction)) + 65))
-                    self.predicted_output.text = result
-                    self.model_used.text = model_text
-                    print(prediction[0])
+    # def predict(self):
+    #     # Predicting the output
+    #     global final_mask,model_text
+    #     prediction, prob = self.predict_model(final_mask)
+    #     if prob >= .80:
+    #         if model == model_alpha:
+    #             if prediction[0] == 26:
+    #                 model_text=self.model_switch(1)
+    #             if prediction[0] == 27:
+    #                 model_text=self.model_switch(2)
+    #             else:
+    #                 result = str(chr(prediction[0] + 65))
+    #                 # result = str(chr(result_map(str(prediction)) + 65))
+    #                 self.predicted_output.text = result
+    #                 self.model_used.text = model_text
+    #                 print(prediction[0])
 
-            if model == model_num:
-                if prediction[0] == 10:
-                    model_text=self.model_switch(1)
-                if prediction[0] == 11:
-                    model_text=self.model_switch(2)
-                else:
-                    result = str(prediction[0])
-                    # result = str(result_map2(str(prediction)))
-                    self.predicted_output.text = result
-                    self.model_used.text = model_text
-        self.sentence.text = self.sentence.text + result        
+    #         if model == model_num:
+    #             if prediction[0] == 10:
+    #                 model_text=self.model_switch(1)
+    #             if prediction[0] == 11:
+    #                 model_text=self.model_switch(2)
+    #             else:
+    #                 result = str(prediction[0])
+    #                 # result = str(result_map2(str(prediction)))
+    #                 self.predicted_output.text = result
+    #                 self.model_used.text = model_text
+    #     self.sentence.text = self.sentence.text + result + "(prob=" + str(prob*100) + "%)"        
 
     def timer_to_predict(self, dt):
         global interval, timer_val
         if timer_val>0:
             timer_val= timer_val - 1
         else:
-            self.predict()
+            # self.predict()
             timer_val = interval
         
         if timer_val == 0:
@@ -278,31 +285,37 @@ class HslSliderApp(GridLayout):
 
     def slider_change_u_hue(self, val):
         global u_hue
-        u_hue = val
-        # print('u_hue=',val)
+        u_hue = int(val)
+        self.u_hue_lbl.text = str(u_hue)
+        
     def slider_change_u_saturation(self, val):
         global u_saturation
-        u_saturation = val
-        # print('u_saturation=', val)
+        u_saturation = int(val)
+        self.u_sat_lbl.text = str(u_saturation)
+        
     def slider_change_u_value(self, val):
         global u_value
-        u_value = val
-        # print('u_value=', val)
+        u_value = int(val)
+        self.u_val_lbl.text = str(u_value)
+        
     def slider_change_l_hue(self, val):
         global l_hue
-        l_hue = val
-        # print('l_hue=', val)
+        l_hue = int(val)
+        self.l_hue_lbl.text = str(l_hue)
+        
     def slider_change_l_saturation(self, val):
         global l_saturation
-        l_saturation = val
-        # print('l_saturation=', val)
+        l_saturation = int(val)
+        self.l_sat_lbl.text = str(l_saturation)
+        
     def slider_change_l_value(self, val):
         global l_value
-        l_value = val
-        # print('l_value=', val)
+        l_value = int(val)
+        self.l_val_lbl.text = str(l_value)
+
     def thresh_change(self,val):
         global t
-        t = val
+        t = int(val)
         self.thresh_lbl.text = str(val) 
 
     def interval_change(self, val):
@@ -314,7 +327,7 @@ class HslSliderApp(GridLayout):
 
     def image(self):
         pop = Popup(title='Hand Signs Reference Chart', content=Image(source='images/texture3.jpg'),
-                    size_hint=(None, None), size=(800, 600))
+                    size_hint=(None, None), size=(1000, 800))
         pop.open()
 
 class SliderApp(App):
